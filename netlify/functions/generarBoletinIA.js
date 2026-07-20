8a831c2a97510ce3a263322c024cc2e60f99976c
serena <sere22giacomelli@gmail.com>
2026-07-20 09:08:12 -0300

    fix

--- netlify/functions/generarBoletinIA.js
+++ netlify/functions/generarBoletinIA.js
@@ -1,3 +1,13 @@
+if (typeof global.DOMMatrix === 'undefined') {
+  global.DOMMatrix = class DOMMatrix {};
+}
+if (typeof global.Path2D === 'undefined') {
+  global.Path2D = class Path2D {};
+}
+if (typeof global.ImageData === 'undefined') {
+  global.ImageData = class ImageData {};
+}
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const cheerio = require('cheerio');
